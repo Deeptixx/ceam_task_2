20e336b185b742c2a246c89f5116ba1205385ca9
Deeptixx <deeptib0730@gmail.com>
2026-06-23 20:59:47 +0530

    train modell

---
+++ training_evaluation/train.py
@@ -0,0 +1,27 @@
+import numpy as np
+from stable_baselines3 import PPO
+import src.agent as agent
+learning_rate=0.0003
+n_steps=2048
+batch_size=64
+epochs=10
+gamma=0.99
+total_timestamps=10000
+max_steps_per_episode=1000
+
+def train_agent():
+    racing_env=agent.RacingEnv()
+    model=PPO("MLpPolicy",
+        racing_env,
+        learning_rate=learning_rate,
+        n_steps=n_steps,
+        batch_size=batch_size,
+        n_epochs=epochs,
+        gamma=gamma,
+        verbose=1
+    )
+    print(f"Training agent for {total_timestamps} timesteps:")
+    model.learn(total_timesteps=total_timestamps)
+    print("Training complete.")
+    return model
+  
