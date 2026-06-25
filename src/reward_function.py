65b181e53f9c641a19ff8b1cbdf011ef974aa47e
Deeptixx <deeptib0730@gmail.com>
2026-06-25 23:02:59 +0530

    temporaray reward function

--- src/reward_function.py
+++ src/reward_function.py
@@ -1,30 +1,9 @@
-def get_reward(crashed, finished, speed): #basic reward function
+def get_reward(crashed, finished, speed):
     if crashed:
-        return -10
-    elif finished:
-        return 100
-    else:
-        reward = 0.1
-    if speed < 1.0:
-        reward -= 0.05
-    return reward
-def get_reward_fast(crashed, finished, speed): #reward fucntion that encourages faster driving
-    if crashed:
-        return -15
-    elif finished:
-        return 150
-    else:
-        reward = 0.2
-    if speed < 2.0:
-        reward -= 0.1
-    return reward
-def get_reward_safe(crashed, finished, speed): #reward function that encourages safer driving.
-    if crashed:
-        return -20
-    elif finished:
-        return 100
-    else:
-        reward = 0.15
-    if speed < 0.5:
-        reward -= 0.02
+        return -50
+    if finished:
+        return 500
+    
+    reward = 0.1
+    reward += speed * 0.2  # Increased from 1.5
     return reward
